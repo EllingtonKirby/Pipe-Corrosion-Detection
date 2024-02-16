d4058bdfd2da149a06f9b6412ff35791a04f84ac
Ellington <ellingtonkirby31@gmail.com>
2024-02-16 19:46:17 +0100

    use cuda

--- evaluate_unet.py
+++ evaluate_unet.py
@@ -6,9 +6,27 @@ from tqdm import tqdm
 import data_pipeline
 import unet
 
+global DEVICE
+DEVICE = torch.device('cpu')
+if torch.cuda.is_available():
+  DEVICE = torch.device('cuda:0')
+  print("CUDA is available and is used")
+elif not torch.backends.mps.is_available():
+  if not torch.backends.mps.is_built():
+    print("MPS not available because the current PyTorch install was not "
+          "built with MPS enabled.")
+  else:
+      print("MPS not available because the current MacOS version is not 12.3+ "
+          "and/or you do not have an MPS-enabled device on this machine.")
+  DEVICE = torch.device('cpu')
+  print("CUDA and MPS are not available, switching to CPU.")
+else:
+  DEVICE = torch.device("mps")
+  print("CUDA not available, switching to MPS")
+
 if __name__ == '__main__':
   model = unet.UNet(n_channels=1, n_classes=1)
-  model.load_state_dict(torch.load('./unet_13.pt', map_location=torch.device('cpu')))
+  model.load_state_dict(torch.load('./unet_13.pt'))
   test_df = data_pipeline.build_test_dataframe(use_processed_images=False)
   train_df = data_pipeline.build_dataframe(use_processed_images=False)
   X_test, X_names, X_train = data_pipeline.build_test_dataloaders(test_df, train_df, apply_scaling=True)
@@ -16,6 +34,7 @@ if __name__ == '__main__':
   predictions = {}
   model.eval()
   for index, x in tqdm(enumerate(test_dl)):
+    x = x.to(DEVICE)
     out = model(x[0])
     preds = (F.sigmoid(out) > .5)*1.
     name = X_names[index][0]
