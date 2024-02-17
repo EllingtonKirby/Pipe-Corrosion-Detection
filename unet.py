0639b5ef4edc75abf76246305e947033ebd0f7f4
Ellington <ellingtonkirby31@gmail.com>
2024-02-17 20:45:36 +0100

    fix unet coe

--- unet.py
+++ unet.py
@@ -16,16 +16,16 @@ class UNet(nn.Module):
         factor = 2 if bilinear else 1
         if n_steps >= 1:
             self.down1 = (Down(64, 128))
-            self.up1 = (Up(1024, 512 // factor, bilinear))
+            self.up4 = (Up(128, 64, bilinear))
         if n_steps >= 2:
             self.down2 = (Down(128, 256))
-            self.up2 = (Up(512, 256 // factor, bilinear))
+            self.up3 = (Up(256, 128 // factor, bilinear))
         if n_steps >= 3:
             self.down3 = (Down(256, 512))
-            self.up3 = (Up(256, 128 // factor, bilinear))
+            self.up2 = (Up(512, 256 // factor, bilinear))
         if n_steps >= 4:
             self.down4 = (Down(512, 1024 // factor))
-            self.up4 = (Up(128, 64, bilinear))
+            self.up1 = (Up(1024, 512 // factor, bilinear))
         self.outc = (OutConv(64, n_classes))
 
     def forward(self, x):
@@ -43,13 +43,13 @@ class UNet(nn.Module):
             x5 = self.down4(x4)
             x = x5
 
-        if self.n_steps >= 1:
+        if self.n_steps >= 4:
             x = self.up1(x, x4)
-        if self.n_steps >= 2:
-            x = self.up2(x, x3)
         if self.n_steps >= 3:
+            x = self.up2(x, x3)
+        if self.n_steps >= 2:
             x = self.up3(x, x2)
-        if self.n_steps >=4 :
+        if self.n_steps >= 1:
             x = self.up4(x, x1)
         
         logits = self.outc(x)
