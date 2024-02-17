fix broken loss averaging

kfolds_unet.py
@@ -45,8 +45,8 @@ def main():
       model_losses.append(valid_losses[-1])
       model_metrics.append(valid_metrics[-1])
 
-    per_model_losses[model.n_steps] = np.mean(model_losses)
-    per_model_metrics[model.n_steps] = np.mean(model_metrics)
+    per_model_losses[model.n_steps] = np.mean(model_losses.cpu().detach())
+    per_model_metrics[model.n_steps] = np.mean(model_metrics.cpu().detach())
     print(f"Finish folds for Model with Steps: {model.n_steps}")
     print(f"Average Loss: {per_model_losses[model.n_steps]}")
     print(f"Average IoU   {per_model_metrics[model.n_steps]}")