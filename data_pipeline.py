ce1ade1941b23f6fca8af74a65ebf10156218a3a
Ellington <ellingtonkirby31@gmail.com>
2024-03-12 23:29:03 +0100

    prep r2u 12 cutout

--- data_pipeline.py
+++ data_pipeline.py
@@ -112,7 +112,7 @@ def image_label_transforms(image, label, flipper):
         image = flipper(image)
         label = flipper(label)
 
-    image, label = cutout(image, label, size=6)
+    image, label = cutout(image, label, size=12)
 
     return image, label
 
