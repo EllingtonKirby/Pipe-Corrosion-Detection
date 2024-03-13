5b5c296f40c42354996abaa6b5daa4a75e3e7841
Ellington <ellingtonkirby31@gmail.com>
2024-03-13 22:52:35 +0100

    kfolds attu with weighting

--- kfolds_r2u_att.py
+++ kfolds_r2u_att.py
@@ -7,12 +7,27 @@ import data_pipeline
 import attunet
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import RobustScaler
+import collections
 
 def main():
   dataframe = data_pipeline.build_dataframe(use_processed_images=False)
   X = torch.from_numpy(np.vstack(dataframe['data'].to_numpy()))
   X = torch.nan_to_num(X)
   Y = torch.from_numpy(np.vstack(dataframe['labels'].to_numpy()))
+  
+  well_numbers = dataframe['well_number']
+  wells = torch.from_numpy(np.vstack(well_numbers.to_numpy())) - 1
+  well_mean_weight = np.mean(well_numbers.value_counts().values)
+  
+  tau = 5
+  sample_weight = {well: ratio/well_mean_weight for well, ratio in well_numbers.value_counts().items()}
+  sample_weight = {well: min(1/ratio, tau) for well, ratio in sample_weight.items()}
+
+  sample_weight = collections.OrderedDict(sorted(sample_weight.items()))
+  print("Well weights: ", sample_weight)
+  sample_weight = torch.tensor(list(sample_weight.values()))
+  wells = sample_weight[wells].flatten()
+
   splits = KFold(n_splits=5, shuffle=True)
 
   models = [2] # Number of Recurrence steps
@@ -23,11 +38,12 @@ def main():
     model_losses = []
     model_metrics = []
     for fold, (train_indices, valid_indices) in enumerate(splits.split(X=X, y=Y)):
-      model = r2u_att.R2AttU_Net(img_ch=1, output_ch=1).to(torch.device('cuda:0'))
+      model = attunet.AttU_Net(img_ch=1, output_ch=1).to(torch.device('cuda:0'))
       print("-"*100)
-      print(f"R2UAtt Net Fold: {fold}")
+      print(f"AttU Net Fold: {fold}")
       X_train, X_valid = X[train_indices].float().reshape(-1, 36*36), X[valid_indices].float().reshape(-1, 36*36)
       Y_train, Y_valid = Y[train_indices].float(), Y[valid_indices].float()
+      Wells_train, Wells_valid = wells[train_indices].float().reshape(-1, 1, 1, 1), wells[valid_indices].float().reshape(-1, 1, 1, 1)
 
       # Scale
       scaler = RobustScaler().fit(X_train)
@@ -35,14 +51,14 @@ def main():
       X_train, X_valid = X_train.float().reshape(-1, 1, 36, 36), X_valid.float().reshape(-1, 1, 36, 36)
       Y_train, Y_valid = Y_train.reshape(-1, 1, 36, 36), Y_valid.reshape(-1, 1, 36, 36)
 
-      train_dataset = data_pipeline.WellsDataset(X_train, Y_train, transform=data_pipeline.image_label_transforms)
-      valid_dataset = data_pipeline.WellsDataset(X_valid, Y_valid, transform=None)
+      train_dataset = data_pipeline.WellsDataset(X_train, Y_train, transform=data_pipeline.image_label_transforms,  wells=Wells_train)
+      valid_dataset = data_pipeline.WellsDataset(X_valid, Y_valid, transform=None, wells=Wells_valid)
 
       train_dataloader = DataLoader(train_dataset, batch_size=128)
       valid_dataloader = DataLoader(valid_dataset, batch_size=128)
       
       # Train
-      _, _, _, valid_losses, valid_metrics = train_r2u_att.train_local(model, train_dataloader, valid_dataloader, lr=.001, num_epochs=50)
+      _, _, _, valid_losses, valid_metrics = train_r2u_att.train_local_weighted(model, train_dataloader, valid_dataloader, lr=.001, num_epochs=50)
       model_losses.append(valid_losses[-1])
       model_metrics.append(valid_metrics[-1])
 
@@ -55,7 +71,7 @@ def main():
   print("-"*100)
   print("Finished full KFolds testing")
   for key in per_model_losses.keys():
-    print(f"R2UAtt(): loss={per_model_losses[key]}, IoU={per_model_metrics[key]}")
+    print(f"AttU(): loss={per_model_losses[key]}, IoU={per_model_metrics[key]}")
 
 if __name__=='__main__':
   main()
--- train_r2u_att.py
+++ train_r2u_att.py
@@ -41,34 +41,39 @@ class DiceLoss(nn.Module):
     return 1 - dice
 
 class DiceBCELoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
+    def __init__(self):
         super(DiceBCELoss, self).__init__()
 
-    def forward(self, inputs, targets, smooth=1):
+    def forward(self, inputs, targets, weights, smooth=1):
+        if weights == None:
+           weights = torch.ones(targets.shape[0], 1, 1, 1).to(DEVICE)
         
         #comment out if your model contains a sigmoid or equivalent activation layer
         inputs = F.sigmoid(inputs)       
         
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
+        inputs_flat = inputs
+        targets_flat = targets
+
+        intersection = (inputs_flat * targets_flat)
+        dice_loss = 1 - (2 * intersection + smooth) / (inputs_flat.sum(dim=0) + targets_flat.sum(dim=0) + smooth)
+        bce_loss = F.binary_cross_entropy(inputs_flat, targets_flat, reduction='none')
+        combined_loss = bce_loss + dice_loss
         
-        intersection = (inputs * targets).sum()                            
-        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
-        Dice_BCE = BCE + dice_loss
-        
-        return Dice_BCE
+        return (combined_loss*weights).sum() / len(targets)
     
 class PseduoLabelBCELoss(nn.Module):
     def __init__(self):
           super(PseduoLabelBCELoss, self).__init__()
 
-    def forward(self, pseudo_label, targets):
+    def forward(self, pseudo_label, targets, weights):
+      if weights == None:
+            weights = torch.ones(targets.shape[0], 1, 1, 1).to(DEVICE)
+          
       tensors = targets.flatten(start_dim=1)
       contains_ones = (tensors == 1).any(dim=1)
       labels = contains_ones.int()
-      return F.binary_cross_entropy_with_logits(pseudo_label, labels.reshape(-1,1).float())
+      bce = F.binary_cross_entropy_with_logits(pseudo_label, labels.view(-1,1).float(), reduction='none')
+      return (bce * weights).sum() / len(targets)
     
 class VerboseReduceLROnPlateau(torch.optim.lr_scheduler.ReduceLROnPlateau):
     def __init__(self, *args, **kwargs):
@@ -182,6 +187,71 @@ def train_local(model: nn.Module, train_dataloader, validation_dataloader, lr, n
         if pseudo_label != None:
           class_loss = pseudo_labeling_criterion(pseudo_label, labels)
         else:
+          class_loss = 0
+        loss = dice_loss + class_loss
+        iou = metric(preds, labels)
+        valid_loss += loss
+        valid_iou += iou
+    
+    train_losses.append(train_loss.item() / len(train_dataloader))
+    train_ious.append(train_iou.item() / len(train_dataloader))
+    valid_losses.append(valid_loss.item() / len(validation_dataloader))
+    valid_ious.append(valid_iou.item() / len(validation_dataloader))
+
+    print(f'Epoch: {e}')
+    print(f'Train loss:      {train_losses[-1]}')
+    print(f'Validation loss: {valid_losses[-1]}')
+    print(f'Train intersection over union:      {train_ious[-1]}')
+    print(f'Validation intersection over union: {valid_ious[-1]}')
+    scheduler.step(valid_losses[-1])
+    
+  return model, train_losses, train_ious, valid_losses, valid_ious
+
+def train_local_weighted(model: nn.Module, train_dataloader, validation_dataloader, lr, num_epochs):
+  metric = BinaryJaccardIndex().to(DEVICE)
+  dice_criterion = DiceBCELoss().to(DEVICE)
+  pseudo_labeling_criterion = PseduoLabelBCELoss()
+  optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
+  scheduler = VerboseReduceLROnPlateau(optimizer, 'min', patience=5)
+
+  train_losses = []
+  train_ious = []
+  valid_losses = []
+  valid_ious = []
+
+  for e in range(num_epochs):
+    train_loss = 0
+    train_iou = 0
+    for input, labels, weights in tqdm(iter(train_dataloader)):
+      input = input.to(DEVICE)
+      labels = labels.to(DEVICE)
+      weights= weights.to(DEVICE)
+      optimizer.zero_grad()
+      preds, pseudo_label = model(input)
+      dice_loss = dice_criterion(preds, labels, weights)
+      if pseudo_label != None:
+        class_loss = pseudo_labeling_criterion(pseudo_label, labels, weights=None)
+      else: 
+        class_loss = 0
+      loss = dice_loss + class_loss
+      iou = metric(preds, labels)
+      train_loss += loss
+      train_iou += iou
+      loss.backward()
+      optimizer.step()
+
+    valid_loss = 0
+    valid_iou = 0
+    with torch.no_grad():
+      for input, labels, weights in tqdm(iter(validation_dataloader)):
+        input = input.to(DEVICE)
+        labels = labels.to(DEVICE)
+        weights= weights.to(DEVICE)
+        preds, pseudo_label = model(input)
+        dice_loss = dice_criterion(preds, labels, weights)
+        if pseudo_label != None:
+          class_loss = pseudo_labeling_criterion(pseudo_label, labels, weights=None)
+        else: 
           class_loss = 0
         loss = dice_loss + class_loss
         iou = metric(preds, labels)
