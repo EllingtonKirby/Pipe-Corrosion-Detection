d7e021b029d733570d4912268b272103612a74a6
Ellington <ellingtonkirby31@gmail.com>
2024-03-13 20:26:18 +0100

    wow i really messed that up lol

--- data_pipeline.py
+++ data_pipeline.py
@@ -229,8 +229,8 @@ def build_dataloaders_weighted(tau):
     wells = torch.from_numpy(np.vstack(well_numbers.to_numpy())) - 1
     well_mean_weight = np.mean(np.log(well_numbers.value_counts().values))
     
-    sample_weight = {well: np.exp(well_mean_weight - ratio) for well, ratio in well_numbers.value_counts().items()}
-    sample_weight = {well: min(1/ratio, tau) for well, ratio in sample_weight.items()}
+    sample_weight = {well: ratio/well_mean_weight for well, ratio in well_numbers.value_counts().items()}
+    sample_weight = {well: min(1/ratio, 5) for well, ratio in sample_weight.items()}
     
     sample_weight = collections.OrderedDict(sorted(sample_weight.items()))
     sample_weight = torch.tensor(list(sample_weight.values()))
